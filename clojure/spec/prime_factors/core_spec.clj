50c936f78d990745fb12c7c9cedfd067e3c33dd0
k-rea <kitahara@k-rea.com>
2024-06-28 08:56:40 +0900

    clojure: add pf of 3

--- clojure/spec/prime_factors/core_spec.clj
+++ clojure/spec/prime_factors/core_spec.clj
@@ -3,6 +3,7 @@
             [prime-factors.core :refer :all]))
 
 (describe "Prime factors"
+          (it "3" (should= [3] (prime-factors-of 3)))
           (it "2" (should= [2] (prime-factors-of 2)))
           (it "1" (should= [] (prime-factors-of 1))))
 (run-specs)
